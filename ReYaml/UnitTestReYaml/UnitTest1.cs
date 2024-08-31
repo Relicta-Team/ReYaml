8b796464dc9a56d76974d21e9a74d51bce99d3e6
Yodes <35444748+Yobas@users.noreply.github.com>
2024-08-31 09:46:59 +0300

    fix unit tests

--- ReYaml/UnitTestReYaml/UnitTest1.cs
+++ ReYaml/UnitTestReYaml/UnitTest1.cs
@@ -3,6 +3,7 @@ using System;
 using System.Text;
 using System.IO;
 using ReYaml;
+using System.Diagnostics;
 
 namespace UnitTestReYaml
 {
@@ -15,22 +16,20 @@ namespace UnitTestReYaml
 		}
 
 
-		private void test_buffer_size(int size)
+		private void test_buffer_size(string textData,string parsedData,int size)
         {
 			Assert.AreEqual(0, getbuffer().Length, "Buffer must be empty");
-
-			int outSize = size;
+			
 			var output = new StringBuilder();
-			var textData = File.ReadAllText("..\\..\\test.yml");
-			var parsedData = File.ReadAllText("..\\..\\parsed.txt");
-			CommandProcessor.ParseCommand(output, outSize, "parse_string", new string[] { textData });
+			
+			CommandProcessor.ParseCommand(output, size, "parse_string", new string[] { textData });
 
-			if (output.Length > outSize)
+			if (parsedData.Length > size)
             {
 				Assert.AreEqual("$PART$", output.ToString(), $"Unexpected part token for buffsize {size}, outlen: {output.Length}");
             } else
             {
-				Assert.AreEqual(parsedData, output.ToString(), $"Result missmatch for buffsize {outSize}");
+				Assert.AreEqual(parsedData, output.ToString(), $"Result missmatch for buffsize {size}; Parsed data len {parsedData.Length}");
 				return;
             }
 
@@ -39,20 +38,20 @@ namespace UnitTestReYaml
 			do
 			{
 				comDat.Clear();
-				CommandProcessor.ParseCommand(comDat, outSize, "has_parts", new string[] { });
+				CommandProcessor.ParseCommand(comDat, size, "has_parts", new string[] { });
 				if (comDat.ToString() == "True")
 				{
 					comDat.Clear();
 					var bufflen = getbuffer().Length;
-					CommandProcessor.ParseCommand(comDat, outSize, "next_read", new string[] { });
+					CommandProcessor.ParseCommand(comDat, size, "next_read", new string[] { });
 					finalOut += comDat;
-					if (bufflen < outSize)
+					if (bufflen < size)
 					{
 						Assert.AreEqual(0, getbuffer().Length);
 					}
 					else
 					{
-						Assert.AreEqual(bufflen, getbuffer().Length + outSize);
+						Assert.AreEqual(bufflen, getbuffer().Length + size);
 					}
 
 				}
@@ -63,16 +62,24 @@ namespace UnitTestReYaml
 
 			} while (getbuffer().Length > 0);
 
-			Assert.AreEqual(parsedData, finalOut, $"Result missmatch for buffsize {outSize}");
+			Assert.AreEqual(parsedData, finalOut, $"Result missmatch for buffsize {size}; PARSED ({parsedData.Length}) != OUTPUT ({finalOut.Length})");
 		}
 
 		[TestMethod]
 		public void PartialParsingTest()
 		{
-			var textData = File.ReadAllText("..\\..\\test.yml");
-			for (int i = 10; i < textData.Length + 10; i++)
+			var textData = File.ReadAllText("..\\..\\test.yml",Encoding.UTF8);
+			var parsedData = File.ReadAllText("..\\..\\parsed.txt", Encoding.UTF8);
+
+			//removing \r char from parsed file (if contains)
+			if (parsedData.Contains("\r"))
+			{
+				parsedData = parsedData.Replace("\r", "");
+			}
+			
+			for (int i = 10; i < (textData.Length + 10); i++)
             {
-				test_buffer_size(i);
+				test_buffer_size(textData,parsedData,i);
             }
 
 		}
